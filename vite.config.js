4687eee4fc3a3370759493c4ba3ce180452e7226
hellraiserrob <robert.phillips1@maersk.com>
2024-07-04 08:56:31 +0100

    gh pages action

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,5 @@
-// import pkg from "./package.json";
-// import fs from "fs";
-// import { defineConfig } from "vite";
-// import { resolve } from "path";
+import { defineConfig } from "vite";
 
-// export default defineConfig({
-//   build: {
-//     lib: {
-//       // formats: ["es", "iife"],
-//       entry: resolve(__dirname, "src/link-later.ts"),
-//       name: "ls",
-//       fileName: "ls"
-//     },
-//   },
-//   plugins: [
-//   ]
-// });
+export default defineConfig({
+  base: "/masonry/"
+});
